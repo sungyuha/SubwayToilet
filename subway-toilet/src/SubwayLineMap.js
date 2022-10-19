e72fc5eaf5734d4850d69dcc60b01553768bfc75
Mokdasoo <ahrektn@gmail.com>
2022-10-20 01:24:45 +0900

    subway font click event and hover simple effect

--- subway-toilet/src/SubwayLineMap.js
+++ subway-toilet/src/SubwayLineMap.js
@@ -1,4 +1,11 @@
+import {useEffect} from 'react';
+
 const SubwayLineMap = () => {
+    useEffect(()=>{
+        const arr = document.getElementById("name_x5F_ko");
+        console.log(arr);   
+    },[]);
+    
     return(
         <object type="image/svg+xml" data="/Seoul_subway_linemap_ko.svg" width='1000px'>수도권 지하철 노선도</object>
     );
