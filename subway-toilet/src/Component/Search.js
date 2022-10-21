d5e4dbb24b66291465cc39905cf2848a997d67ac
YUHASUNG <yuha822@naver.com>
2022-10-21 13:22:27 +0900

    Search 검색 드롭다운

--- subway-toilet/src/Component/Search.js
+++ subway-toilet/src/Component/Search.js
@@ -1,13 +1,63 @@
-import React from 'react';
+import React, { useState } from "react";
+import './Search.scss';
+//import {ReactComponent as SubwayLine2} from './Seoul_subway_linemap_ko.svg';
+import SearchIcon from '@mui/icons-material/Search';
+import CloseIcon from '@mui/icons-material/Close';
 
-function Search() {
-    return(
-        <div>
-            <input type="text" placeholder="역명을 검색하세요." />
-            <button type="button">검색</button>
+function Search({data}) {
+    const [filteredData, setFilteredData] = useState([]);
+    const [wordEntered, setWordEntered] = useState("");
+
+    const handleFilter = (event) => {
+        const searchWord = event.target.value;
+        setWordEntered(searchWord);
+        console.log( data.DATA );
+        const newFilter = data.DATA.filter((value) => {
+            return value.station_nm.includes(searchWord);
+        });
+
+        if (searchWord === "") {
+        setFilteredData([]);
+        } else {
+        setFilteredData(newFilter);
+        }
+    };
+
+    const clearInput = () => {
+        setFilteredData([]);
+        setWordEntered("");
+    };
+
+    return (
+        <div className="search">
+        <div className="searchInputs">
+            <input
+            type="text"
+            placeholder="역명을 검색하세요."
+            value={wordEntered}
+            onChange={handleFilter}
+            />
+            <div className="searchIcon">
+            {filteredData.length === 0 ? (
+                <SearchIcon />
+            ) : (
+                <CloseIcon id="clearBtn" onClick={clearInput} />
+            )}
+            </div>
         </div>
-    )
+        {filteredData.length !== 0 && (
+            <div className="dataResult">
+            {filteredData.slice(0, 15).map((value, key) => {
+                return (
+                <a className="dataItem" href={value.link} target="blank">
+                    <p>{value.station_nm} </p>
+                </a>
+                );
+            })}
+            </div>
+        )}
+        </div>
+    );
 }
 
 export default Search;
-
