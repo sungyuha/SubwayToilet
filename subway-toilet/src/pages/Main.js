43aaadba06ba0eec3054201624fa57e2c8185b87
sungyuha <yuha822@naver.com>
2022-10-21 18:35:12 +0900

    Search

--- subway-toilet/src/pages/Main.js
+++ subway-toilet/src/pages/Main.js
@@ -1,11 +1,16 @@
 import React from "react";
-import MainNotice from "../components/MainNotice";
-import MainSuggest from "../components/MainSuggest";
+import MainNotice from "../Components/MainNotice";
+import MainSuggest from "../Components/MainSuggest";
+import Search from "../Components/Search";
+import SubwayData from "../Subway-map.json";
+import SubwayLine from "../SubwayLineMap";
 
 const Main = () => {
   return (
     <>
       <div>
+        <Search data={SubwayData} />
+        <SubwayLine />
         <MainNotice />
         <MainSuggest />
       </div>
