a489363f1450b1476e1f7ffa72e83de9aba7e9d4
Ethanolll <108229203+Ethanolll@users.noreply.github.com>
2022-10-22 23:19:04 +0900

    메인페이지 공지사항 건의사항 scss 수정

--- subway-toilet/src/pages/Main.js
+++ subway-toilet/src/pages/Main.js
@@ -1,11 +1,12 @@
 import React from "react";
 import MainNotice from "../Components/MainNotice";
 import MainSuggest from "../Components/MainSuggest";
+import styles from "./Main.module.scss";
 
 const Main = () => {
   return (
     <>
-      <div>
+      <div className={styles.main__content}>
         <MainNotice />
         <MainSuggest />
       </div>
