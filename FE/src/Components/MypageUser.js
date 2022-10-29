e2e709bd990c26a72dc4d4caeccddb2d7cb704d7
YUHASUNG <yuha822@naver.com>
2022-10-29 17:17:32 +0900

    mypage 디자인

--- FE/src/Components/MypageUser.js
+++ FE/src/Components/MypageUser.js
@@ -12,14 +12,13 @@ const MypageUser = () => {
     });
 
     const { name, id, password, email } = inputs;
-    const onChangeText = (e) => {
+    const onChangeMypage = (e) => {
         console.dir(e.target.value);
         const { value, name } = e.target; // e.target에서 value와 name 추출
         setInputs({
           ...inputs, // 기존의 input 객체를 복사(불변성을 위해)
           [name]: value, // name 키를 가진 값을 value 로 변경
         });
-        MypageUserHandler();
     }
 
     const MypageUserHandler = (e) => {
@@ -49,11 +48,11 @@ const MypageUser = () => {
             <h1 className="user">회원정보</h1>
             <div className="form-wrap">
                 <form onSubmit={MypageUserHandler}>
-                    <input type="text" placeholder='이름' value={name}onChange={onChangeText} /><br />
-                    <input type="text" placeholder='아이디' value={id} onChange={onChangeText} /><br />
-                    <input type="password" placeholder='비밀번호' value={password} onChange={onChangeText} /><br />
-                    <input type="email" placeholder='이메일' value={email} onChange={onChangeText} /><br />
-                    <button type="submit" className="btn">회원정보수정</button>
+                    <input type="text" placeholder='이름' value={name} onChange={onChangeMypage} /><br />
+                    <input type="text" placeholder='아이디' value={id} onChange={onChangeMypage} /><br />
+                    <input type="password" placeholder='비밀번호' value={password} onChange={onChangeMypage} /><br />
+                    <input type="email" placeholder='이메일' value={email} onChange={onChangeMypage} /><br />
+                    <button type="submit" className="mypage-btn">회원정보수정</button>
                     <div>
                         <span className="delete">
                             회원탈퇴
