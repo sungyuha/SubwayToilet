8b88e598faec42679cf88b37f53a9965b4938d5d
Ethanolll <icedgreentea676@gmail.com>
2022-10-31 22:33:53 +0900

    mpdal UI 수정

--- FE/src/Components/Modal/ModalContent/ModalContentInfo.js
+++ FE/src/Components/Modal/ModalContent/ModalContentInfo.js
@@ -10,51 +10,46 @@ function ModalContentInfo({toilet}) {
   return (
     <>
       <div className="modal_content_info">
-        <div className="modal_content_info_title">
-          <p className="modal_content_info_title_title">화장실 정보</p>
-        </div>
-        <div className="modal_content_info_content">
-          <ul>
-
-            
-
-            {/* {
-              toilet.normal.length ? toilet.normal.map((item, index)=>(
-                <li>
-                  <p>
-                    <img src={turnstiles} />
-                    
-                    개찰구 내 화장실 
-                    
-                    
-                    <span> / 위치 :</span>
-                  </p>
-                </li>
-              )) : <></> } */}
-            
-
-            <li>
-              <p className="modal_content_info_content_title">
-                {/* <img src={turnstiles} /> */}
-                일반 화장실
-              </p>
-              <div className="modal_content_info_content_able">
-                <p>개찰구 정보</p>
-                <p>화장실 위치</p>
-              </div>
-            </li>
-
-            <li>
-              <p className="modal_content_info_content_title">
-                {/* <img src={disabled} /> */}
-                장애인 화장실
-              </p>
-              <div className="modal_content_info_content_able">
-                <p>개찰구 정보</p>
-                <p>화장실 위치</p>
-              </div>
-            </li>
-          </ul>
+        <div className="modal_content_info_box">
+          <div className="modal_content_info_title">
+            <p className="modal_content_info_title_title">화장실 정보</p>
+          </div>
+          <div className="modal_content_info_content">
+            <ul>
+              <li>
+                <p className="modal_content_info_content_title">
+                  {/* <img src={turnstiles} /> */}
+                  일반 화장실
+                </p>
+                <div className="modal_content_info_content_able">
+                  <div className="modal_content_info_content_able_info">
+                    <p>개찰구</p>
+                    <p>개찰구 내</p>
+                  </div>
+                  <div className="modal_content_info_content_able_info">
+                    <p>위치</p>
+                    <p>~~~~~~~~~~~~~~~~~~</p>
+                  </div>
+                </div>
+              </li>
+              <li>
+                <p className="modal_content_info_content_title">
+                  {/* <img src={disabled} /> */}
+                  장애인 화장실
+                </p>
+                <div className="modal_content_info_content_able">
+                  <div className="modal_content_info_content_able_info">
+                    <p>개찰구</p>
+                    <p>개찰구 내</p>
+                  </div>
+                  <div className="modal_content_info_content_able_info">
+                    <p>화장실 위치</p>
+                    <p>~~~~~~~~~~~~~~~~~~~</p>
+                  </div>
+                </div>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
     </>
