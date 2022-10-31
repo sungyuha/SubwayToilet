4dd5f32bbb994574ba8fa498205b073ada41da9e
Ethanolll <icedgreentea676@gmail.com>
2022-10-31 18:58:14 +0900

    모달 리뷰 작성

--- FE/src/Components/Modal/StarRating.js
+++ FE/src/Components/Modal/StarRating.js
@@ -14,7 +14,7 @@ const StarRating = () => {
                 return (
                     <label>
                         <input type="radio" name='rating' value={ratingValue} onClick={() => setRating(ratingValue)} />
-                        <FaStar className='star' color={ratingValue <= (hover || rating) ? "#ffc107" : "#e4e5e9"} size={50} onMouseEnter={() => setHover(ratingValue)} onMouseLeave={() => setHover(null)}/>
+                        <FaStar className='star' color={ratingValue <= (hover || rating) ? "#ffc107" : "#e4e5e9"} size={20} onMouseEnter={() => setHover(ratingValue)} onMouseLeave={() => setHover(null)}/>
                     </label>
                 );
             })}
