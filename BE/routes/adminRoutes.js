312ebd40ab976b8f4adbd669fbcdc72795b3d0d0
user <user@userui-iMac.local>
2022-10-31 22:43:08 +0900

    token_review

--- BE/routes/adminRoutes.js
+++ BE/routes/adminRoutes.js
@@ -1,9 +1,9 @@
 const { Router } = require('express');
 const router = Router();
-
+const passport = require('../passport/index.js');
 const adminController = require('../controller/adminController');
 
-router.get('/', adminController.getAdmin);
+router.get('/', passport.authenticate('jwt', {session:false}), adminController.getAdmin);
 router.get('/lines', adminController.getLines);
 
 module.exports = router;
--- BE/routes/reviewRoutes.js
+++ BE/routes/reviewRoutes.js
@@ -1,7 +1,21 @@
 const { Router } = require('express');
 const router = Router();
-
+const passport = require('../passport/index.js');
 const reviewControllers = require('../controller/reviewController');
+
+router.get('*', passport.authenticate('jwt', {session:false}), 
+  async (req,res,next) => {
+    console.log("hi");
+    // console.log(req)
+    try {
+      req.decoded = jwt.verify(req.headers.authorization, process.env.JWT_SECRET)
+      next();
+    } catch(error) {
+      console.error(error);
+      next(error);
+    }
+  }
+);
 
 router.post('/', reviewControllers.postReview);
 router.patch('/', reviewControllers.patchReview);
