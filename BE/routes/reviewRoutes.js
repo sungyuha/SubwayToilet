bd7d52d6cb1a9c999da217944219169df5eaff6e
GeunYoung Jang <zzbtang@gmail.com>
2022-11-01 14:31:19 +0900

    token/server

--- BE/routes/reviewRoutes.js
+++ BE/routes/reviewRoutes.js
@@ -1,24 +1,62 @@
 const { Router } = require('express');
 const router = Router();
-const passport = require('../passport/index.js');
 const reviewControllers = require('../controller/reviewController');
+const passport = require('../passport/index');
+const jwt = require('jsonwebtoken');
 
-router.get('*', passport.authenticate('jwt', {session:false}), 
-  async (req,res,next) => {
-    console.log("hi");
+router.post(
+  '/',
+  passport.authenticate('jwt', { session: false }),
+  (req, res, next) => {
+    console.log(req.body);
+    console.log('hi');
     // console.log(req)
     try {
-      req.decoded = jwt.verify(req.headers.authorization, process.env.JWT_SECRET)
+      req.decoded = jwt.verify(req.headers.authorization, process.env.TOKEN);
+      console.log('a', req.decoded);
       next();
-    } catch(error) {
+    } catch (error) {
+      console.log(error);
+      next(error);
+    }
+  },
+  reviewControllers.postReview
+);
+router.patch(
+  '/',
+  passport.authenticate('jwt', { session: false }),
+  (req, res, next) => {
+    console.log(req.body);
+    console.log('hi');
+    // console.log(req)
+    try {
+      req.decoded = jwt.verify(req.headers.authorization, process.env.TOKEN);
+      console.log('a', req.decoded);
+      next();
+    } catch (error) {
       console.error(error);
       next(error);
     }
-  }
+  },
+  reviewControllers.patchReview
 );
-
-router.post('/', reviewControllers.postReview);
-router.patch('/', reviewControllers.patchReview);
-router.delete('/', reviewControllers.deleteReview);
+router.delete(
+  '/',
+  passport.authenticate('jwt', { session: false }),
+  (req, res, next) => {
+    console.log(req.body);
+    console.log('hi');
+    // console.log(req)
+    try {
+      req.decoded = jwt.verify(req.headers.authorization, process.env.TOKEN);
+      console.log('a', req.decoded);
+      next();
+    } catch (error) {
+      console.error(error);
+      next(error);
+    }
+  },
+  reviewControllers.deleteReview
+);
 
 module.exports = router;
