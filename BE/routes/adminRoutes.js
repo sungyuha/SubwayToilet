8e0326939adc0d4ea1ffb3c44756dd89c726850e
Mokdasoo <ahrektn@gmail.com>
2022-11-01 21:48:02 +0900

    admin page create

--- BE/routes/adminRoutes.js
+++ BE/routes/adminRoutes.js
@@ -3,25 +3,7 @@ const router = Router();
 const passport = require('../passport/index.js');
 const adminController = require('../controller/adminController');
 
-router.get(
-  '*',
-  passport.authenticate('jwt', { session: false }),
-  (req, res, next) => {
-    console.log(req.body);
-    console.log('hi');
-    // console.log(req)
-    try {
-      req.decoded = jwt.verify(req.headers.Authorization, process.env.TOKEN);
-      if(req.decoded==='admin')
-        console.log('a', req.decoded);
-        next();
-    } catch (error) {
-      console.log('a');
-      console.log(error);
-      // next(error);
-    }
-  }
-);
+
 router.get('/', adminController.getAdmin);
 router.get('/lines', adminController.getLines);
 
