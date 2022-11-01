2fd424f51c0f28308ffb44ba30427d2e0e0c24ba
Ethanolll <108229203+Ethanolll@users.noreply.github.com>
2022-11-01 17:26:20 +0900

    모달 scss 수정

--- FE/src/Components/Modal/ModalContent/ModalContentInfo.js
+++ FE/src/Components/Modal/ModalContent/ModalContentInfo.js
@@ -12,42 +12,39 @@ function ModalContentInfo({toilet}) {
           <p className="modal_content_info_title_title">화장실 위치</p>
         </div>
         <div className="modal_content_info_content">
+          <div>
+            <ul>
+              <li key={'normal'}>
+                <p className="modal_content_info_content_title">
+                  {/* <img src={turnstiles} /> */}
+                  일반 화장실
+                </p>
+                {
+                  toilet.normal.length ? toilet.normal.map((item, index)=>(
+                    <div key={index} className="modal_content_info_content_able">
+                    <p>개찰구 정보 : 개찰구 {item.gateInotDvNm}부 위치</p>
+                    <p>화장실 위치 : {item.dtlLoc}</p>
+                  </div>
+                )) : <div className="modal_content_info_content_disabled">정보 없음(업데이트 필요)</div> }
+              </li>
 
-          <ul>
-
-            <li key={'normal'}>
-              <p className="modal_content_info_content_title">
-                {/* <img src={turnstiles} /> */}
-                일반 화장실
-              </p>
-              {
-              toilet.normal.length ? toilet.normal.map((item, index)=>(
-                <div key={index} className="modal_content_info_content_able">
-                  <p>개찰구 정보 : 개찰구 {item.gateInotDvNm}부 위치</p>
-                  <p>화장실 위치 : {item.dtlLoc}</p>
-                </div>
-              )) : <div className="modal_content_info_content_disabled">정보 없음(업데이트 필요)</div> }
-              
-            </li>
-
-            <li key='disabled'>
-              <p className="modal_content_info_content_title">
-                {/* <img src={disabled} /> */}
-                장애인 화장실
-              </p>
-              {
-              toilet.disabled.length ? toilet.disabled.map((item, index)=>(
-                <div key={index} className="modal_content_info_content_able">
-                  <p>개찰구 정보 : 개찰구 {item.gateInotDvNm}부 위치</p>
-                  <p>화장실 위치 : {item.dtlLoc}</p>
-                </div>
-              )) : <div className="modal_content_info_content_disabled">정보 없음(업데이트 필요)</div> }
-              
-            </li>
-          </ul>
+              <li key='disabled'>
+                <p className="modal_content_info_content_title">
+                  {/* <img src={disabled} /> */}
+                  장애인 화장실
+                </p>
+                {
+                  toilet.disabled.length ? toilet.disabled.map((item, index)=>(
+                    <div key={index} className="modal_content_info_content_able">
+                    <p>개찰구 {item.gateInotDvNm}부 위치</p>
+                    <p>{item.dtlLoc}</p>
+                  </div>
+                )) : <div className="modal_content_info_content_disabled">정보 없음(업데이트 필요)</div> }       
+              </li>
+            </ul>
+          </div>
          </div>
         </div>
-      
     </>
   );
 }
